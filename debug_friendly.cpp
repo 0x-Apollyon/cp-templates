3894a4700dc973a52360eeb896c09be5a48f4734
Apollyon <149272988+0x-Apollyon@users.noreply.github.com>
2026-04-24 19:20:54 +0530

    Create debug_friendly.cpp

---
+++ debug_friendly.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+
+
+using namespace std;
+
+
+#define ll long long 
+#define ld long double
+
+// use this for fast debugging, doesnt supported nested containers
+template <typename Container>
+void print_all(const Container& cont) {
+    for (const auto& elem : cont) {
+        std::cout << elem << ' ';
+    }
+    std::cout << '\n';
+}
+
+//-------------------- main part starts here --------------------
+
+void solve(){
+    //solve here
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    
+    int testcases;
+    cin >> testcases;
+    
+
+    for (int i = 0; i < testcases; i++){
+        solve();
+    }
+}
