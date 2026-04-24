523051a02eb30b0e1de2baa339e8788d4d29bae0
Apollyon <149272988+0x-Apollyon@users.noreply.github.com>
2026-04-24 19:07:17 +0530

    Add basic.cpp with main function and solve placeholder

---
+++ basic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+    //solve here
+}
+
+int main(){
+    int testcases;
+    cin >> testcases;
+
+    for (int i = 0; i < testcases; i++){
+        solve();
+    }
+}
